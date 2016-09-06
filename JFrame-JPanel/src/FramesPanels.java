946425d6970583dcae6f18219c474f5e4969fe2e
Panjos <panjos@J01FATI-NB00147.J01.Jequiti.com.br>
2016-09-06 09:19:29 -0300

    Criando classe para testes do pacote swing - Criando primeira janela

---
+++ JFrame-JPanel/src/FramesPanels.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class FramesPanels {
+	
+	public static void main(String[] args) {
+		JFrame janela = new JFrame("Window");
+		janela.setSize(400, 300);
+		janela.setVisible(true);
+		
+	}
+
+}
