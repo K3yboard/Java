3c212e5049b1c2a7b70d2a79abd215067b7994da
Paulo Pereira dos Anjos <panjos@J01.Jequiti.com.br>
2016-09-06 14:18:13 -0300

    Adicionando classe para calcular o raio de um circulo

--- POO/src/calculo/Teste.java
+++ POO/src/calculo/Teste.java
@@ -5,6 +5,9 @@ class Teste {
 	public static void main(String[] args) {
 		AreaCalculavel a = new Retangulo(3,2);
 		System.out.println(a.calculaArea());
+		
+		AreaCalculavel circulo = new Circulo(5);
+		System.out.println(circulo.calculaArea());
 	}
 
 }
