103aa39c0292aac3b957e3b968c119fc4d186471
Paulo Pereira dos Anjos <paulo.desenvolvedorjava@gmail.com>
2016-10-22 23:01:35 -0200

    Corrigindo botao de fechar - DisposeOnClose

--- APS_2_2016/src/br/unip/sicc/main/TelaImagem.java
+++ APS_2_2016/src/br/unip/sicc/main/TelaImagem.java
@@ -17,7 +17,7 @@ public class TelaImagem extends JFrame {
 	private JPanel contentPane;
 
 	public TelaImagem() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
